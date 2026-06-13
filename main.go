68775e1e489635f2dbb29d2ad513ec9c30760ee7
zawmoewin120298-collab <zawmoewin120298@gmail.com>
2026-06-13 10:12:54 +0700

    Update startup message for Nova Proxy Web

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gookit/color"
 )
 
 func main() {
-	color.Green.Println("Nova Proxy Web is starting...")
-	// Your proxy logic goes here
+	color.Green.Println("Nova Proxy Web is starting successfully...")
 }
-
